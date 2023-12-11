df69a65155fd509ac25f83ae7d71b6ec909453a2
HS <HS@HS>
2023-12-11 15:04:40 +0800

    20231211

--- Startup.cs
+++ Startup.cs
@@ -23,6 +23,18 @@ namespace DB2VM
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder
+                        .SetIsOriginAllowed(origin => true)
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+
+            });
             services.AddControllers();
         }
 
@@ -44,6 +56,17 @@ namespace DB2VM
             {
                 endpoints.MapControllers();
             });
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyHeader().AllowAnyMethod().SetIsOriginAllowed(_ => true).AllowCredentials();
+            });
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseCors(); // ±Ò¥ÎCORS
+            app.UseAuthorization();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseWebSockets();
         }
     }
 }
